acb8007ed6aa26452177c084485e4d36b92378d3
Antonio <antoniotropiano@gmail.com>
2020-05-25 09:50:36 +0300

    add the requirements and update settings

--- rentit/settings.py
+++ rentit/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -125,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku (for heroku deploy configuration).
+django_heroku.settings(locals())
